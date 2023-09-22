aa46e251475324520d06837caa6a2f685b894d45
Jubayer1228 <jubayer_1228@yahoo.com>
2023-09-22 03:11:26 -0600

    Update urls.py

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,10 @@
+# import necessary packages as needed.
+# we also need to import the views.py as to include urlspattern, we need to call the functions on the views.py
 from django.urls import path
 from . import views
 
-
+# add all the urls here, path function takes three arguments. first, we need to add the urls, then we need to call the function and finally we can give a name of the ursl.
+# we can add optional arguments too.
 urlpatterns = [
     path('customers/', views.customers, name='customers'),
     path('customers/add/', views.add, name= 'add'),
