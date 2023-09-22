91be83ca5b06270bc317ce73e9bda383baf1c15c
Jubayer1228 <jubayer_1228@yahoo.com>
2023-09-22 05:39:14 -0600

    Update models.py

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# This will create a table in the database, This Customer table has six attribute. Django supports serialization which actually converts python
+# object to json or xml and vice versa. Django rest framework has more flexible serialization to handle more complex objects.
 class Customer(models.Model):
   firstname = models.CharField(max_length=255)
   lastname  = models.CharField(max_length=255)
